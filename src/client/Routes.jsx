abfcffeb05f062825f1a1dab67e2f5a7113070d9
VictoryPark <koreacoolar@gmail.com>
2020-02-08 16:35:01 +0900

    deleted temp file and update Router with HashRouter

--- src/client/Routes.jsx
+++ src/client/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
 
-import { Details, Board, Test } from './components';
+import { Board, Test } from './components';
 
 import Header from './components/template/header/Header';
 import Main from './components/main/Main';
@@ -9,16 +9,15 @@ import Footer from './components/template/footer/Footer';
 import { DefaultLayout } from './containers';
 
 export default () => (
-  <BrowserRouter>
+  <HashRouter>
     <Header />
     <Redirect exact from="/" to="/main" />
     <Route exact path="/main" component={Main} />
     <Route path="/board" component={Board} />
-    <Route path="/details" component={Details} />
     <Route path="/test" component={Test} />
 
     <Footer />
 
     <Route path="/ui" component={DefaultLayout} />
-  </BrowserRouter>
+  </HashRouter>
 );
--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,7 +1,6 @@
 import Main from './main/Main';
 import Board from './Board';
-import Details from './Details';
 import Test from './Test';
 import Header from './template/header/Header';
 
-export { Main, Board, Details, Test, Header };
+export { Main, Board, Test, Header };
